c130fa32ee47d97ba887af2a3f6cd88edb6b7ed0
MubarakXBT <Kazeemmubarak@gmail.com>
2024-06-03 23:11:15 +0100

    pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,10 +6,13 @@ returns information about his/her TODO list progress.
 Requirements:
     You must use urllib or requests module
     The script must accept an integer as a parameter, which is the employee ID
-    The script must display on the standard output the employee TODO list progress in this exact format:
+    The script must display on the standard output the TODO list progress
 
-    First line: Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-    Second and N next lines display the title of completed tasks: TASK_TITLE (with 1 tabulation and 1 space before the TASK_TITLE)
+    in this exact format:
+    First line: Employee EMPLOYEE_NAME is done with
+    tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
+    Second and N next lines: display the title of completed tasks: TASK_TITLE
+    (with 1 tabulation and 1 space before the TASK_TITLE)
 """
 import sys
 import requests
@@ -34,6 +37,7 @@ if __name__ == "__main__":
         if task["completed"] is True:
             task_title.append(task["title"])
             done += 1
-    print("Employee {} is done with tasks({}/{}):".format(name, done, total_task))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done, total_task))
     for task in task_title:
         print("\t", task)
